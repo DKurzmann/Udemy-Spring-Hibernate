24ea0451581f73147c009271e3e54bfd370a7683
David Kurzmann <david.g.kurzmann@gmail.com>
2019-09-27 09:11:45 +0200

    IoC und DI

---
+++ spring-demo-one/src/com/luv2code/springdemo/FootballCoach.java
@@ -0,0 +1,16 @@
+package com.luv2code.springdemo;
+
+public class FootballCoach implements Coach{
+	
+	@Override
+	public String getDailyWorkout() {
+		return "30 minutes wall kicking";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
