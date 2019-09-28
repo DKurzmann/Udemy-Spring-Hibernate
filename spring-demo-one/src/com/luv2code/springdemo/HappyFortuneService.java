47e12a28c6aeccd6db497353f36cf651ac74542a
David Kurzmann <david.g.kurzmann@gmail.com>
2019-09-28 13:46:10 +0200

    inject values from properties file

--- spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
+++ spring-demo-one/src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,10 +1,24 @@
 package com.luv2code.springdemo;
 
-public class HappyFortuneService implements FortuneService {
+import java.util.Random;
 
+public class HappyFortuneService implements FortuneService {
+	String Spruch1;
+	String Spruch2;
+	String Spruch3;
+	
+	String[] fortuneList= {Spruch1,Spruch2,Spruch3};
+
+	
+	Random random = new Random();
+	
 	@Override
 	public String getFortune() {
-		return "Today is your lucky day!";
+		return fortuneList[random.nextInt(fortuneList.length-1)];
 	}
 
 }
+
+
+
+
